e6c667fc1ef48ffc66024037f4c0644b0d146d64
Fernando Cusco <cuzcator@gmail.com>
2019-06-30 01:22:29 -0500

    creando la migracion para el modelo autor

--- apps/libro/models.py
+++ apps/libro/models.py
@@ -4,8 +4,7 @@ from django.db import models
 
 class Autor(models.Model):
     id = models.AutoField(primary_key = True)
-    nombre = models.CharField(max_length = 255, blank = False, null, False)
-    apellidos = models.CharField(max_length = 255, blank = False, null, False)
-    nacionalidad = models.CharField(max_length = 100, blank = False, null, False)
-    descripcion = models.TextField(blank = False, null, False)
-    
+    nombre = models.CharField(max_length = 255, blank = False, null = False)
+    apellidos = models.CharField(max_length = 255, blank = False, null = False)
+    nacionalidad = models.CharField(max_length = 100, blank = False, null = False)
+    descripcion = models.TextField(blank = False, null = False)
