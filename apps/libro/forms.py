71bee112dceb02f5cf475fd3526fffb2337a2c30
Fernando Cusco <cuzcator@gmail.com>
2019-07-01 08:35:03 -0500

    Creando el form para Autor

---
+++ apps/libro/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Autor
+
+class AutorForm(forms.ModelForm):
+    class Meta:
+        model = Autor                                                   #definimos el modelo
+        fields = ['nombre', 'apellidos', 'nacionalidad', 'descripcion'] #campos para interactuar, dentro de una lista
+        
