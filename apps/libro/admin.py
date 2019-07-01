06281b6253f25ed3791f8171a7d5e637a1d4f462
Fernando Cusco <cuzcator@gmail.com>
2019-07-01 08:23:54 -0500

    Cambiando la relacion de muchos a muchos en los autores

--- apps/libro/admin.py
+++ apps/libro/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from .models import Autor
+from .models import Libro
 # Register your models here.
 
 admin.site.register(Autor)
+admin.site.register(Libro)
--- apps/libro/models.py
+++ apps/libro/models.py
@@ -8,11 +8,30 @@ class Autor(models.Model):
     apellidos = models.CharField('Apellidos', max_length = 255, blank = False, null = False)
     nacionalidad = models.CharField('Nacionalidad', max_length = 50, blank = False, null = False)
     descripcion = models.CharField('Descripcion', max_length = 50, blank = True, null = True)
+    fecha_creacion = models.DateField('Fecha de creacion', auto_now = True, auto_now_add = False)
 
     class Meta:
         verbose_name = 'Autor'
         verbose_name_plural = 'Autores'
         ordering = ['nombre']
 
-    def __str__(self):
+    def __str__(self):   #nombre del campo no como Object
         return self.nombre
+
+
+class Libro(models.Model):
+    id = models.AutoField(primary_key = True)
+    titulo = models.CharField('Titulo', max_length = 255, blank = False, null = False)
+    fecha_creacion = models.DateField('Fecha de creacion', auto_now = True, auto_now_add = False)
+    fecha_publicacion = models.DateField('Fecha de publicacion', blank = False, null = False)
+    #autor_id = models.ForeignKey(Autor, on_delete=models.CASCADE) #relacion uno a muchos
+    #autor_id = models.OneToOneField(Autor, on_delete=models.CASCADE) #relacion uno a uno
+    autor_id = models.ManyToManyField(Autor) #relacion muchos a muchos
+
+    class Meta:
+        verbose_name = 'Libro'
+        verbose_name_plural = 'Libros'
+        ordering = ['titulo']
+
+    def __str__(self):      #nombre del campo no como Object
+        return self.titulo
