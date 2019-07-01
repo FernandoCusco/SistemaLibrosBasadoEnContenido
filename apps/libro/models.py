8b49c738d0afbddecf5ae8e6aaa8e4da6e2c52ff
Fernando Cusco <cuzcator@gmail.com>
2019-07-01 07:42:45 -0500

    Cambiando el tipo de campo de descripcion del modelo Autor

--- apps/libro/models.py
+++ apps/libro/models.py
@@ -4,14 +4,15 @@ from django.db import models
 
 class Autor(models.Model):
     id = models.AutoField(primary_key = True)
-    nombre = models.CharField(max_length = 255, blank = False, null = False)
-    apellidos = models.CharField(max_length = 255, blank = False, null = False)
-    nacionalidad = models.CharField(max_length = 50, blank = False, null = False)
-    descripcion = models.TextField(blank = False, null = False)
+    nombre = models.CharField('Nombres',max_length = 255, blank = False, null = False)
+    apellidos = models.CharField('Apellidos', max_length = 255, blank = False, null = False)
+    nacionalidad = models.CharField('Nacionalidad', max_length = 50, blank = False, null = False)
+    descripcion = models.CharField('Descripcion', max_length = 50, blank = True, null = True)
 
     class Meta:
         verbose_name = 'Autor'
         verbose_name_plural = 'Autores'
+        ordering = ['nombre']
 
     def __str__(self):
         return self.nombre
