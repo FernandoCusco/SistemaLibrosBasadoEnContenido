4f5f0f104827f733929d5370c5fb84aa4b71232a
Fernando Cusco <cuzcator@gmail.com>
2019-07-03 15:20:48 -0500

    agregando el editar y eliminar Autor

--- apps/libro/urls.py
+++ apps/libro/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
-from .views import crearAutor, listarAutor
+from django.urls import path, re_path
+from .views import crearAutor, listarAutor, editarAutor, eliminarAutor
 
 urlpatterns = [
     path('crear_autor/', crearAutor, name = 'crear_autor'),
-    path('listar_autores/', listarAutor, name = 'listar_autores')
+    path('listar_autores/', listarAutor, name = 'listar_autores'),
+    path('editar_autor/<int:id>', editarAutor, name = 'editar_autor'),
+    path('eliminar_autor/<int:id>', eliminarAutor, name = 'eliminar_autor')
 ]
